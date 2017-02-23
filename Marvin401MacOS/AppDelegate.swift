78a128e0503fe78f1380183c86c5fb34966e97ff
Mazen Azar <mjazar12@gmail.com>
2017-02-23 10:13:53 -0800

    Initial Commit

---
+++ Marvin401MacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Marvin401MacOS
+//
+//  Created by Mazen Azar on 2/23/17.
+//  Copyright © 2017 Mazen Azar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Marvin401MacOSTests/Marvin401MacOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Marvin401MacOSTests.swift
+//  Marvin401MacOSTests
+//
+//  Created by Mazen Azar on 2/23/17.
+//  Copyright © 2017 Mazen Azar. All rights reserved.
+//
+
+import XCTest
+@testable import Marvin401MacOS
+
+class Marvin401MacOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
