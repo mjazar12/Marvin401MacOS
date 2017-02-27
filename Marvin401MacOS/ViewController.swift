a7e1cf710ecc0c6113a85320f10f5d22963b66df
Courtney-Lynn Presto <owner@10.123.177.71>
2017-02-26 21:56:41 -0800

    Courtney Scene

--- Marvin401MacOS/ViewController.swift
+++ Marvin401MacOS/ViewController.swift
@@ -9,24 +9,30 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    @IBOutlet weak var foodCheckButton: NSButton!
+    @IBOutlet weak var transportationCheckButton: NSButton!
     
-    @IBOutlet weak var needTableView: NSTableView!
+    @IBOutlet weak var medicationCheckButton: NSButton!
     
-    let sizeFormatter = ByteCountFormatter()
-    var directory: Directory?
-    var directoryItems: [Metadata]?
-    var sortOrder = Directory.FileOrder.Name
-    var sortAscending = true
-
+    @IBOutlet weak var socializationCheckButton: NSButton!
+    
+    @IBOutlet weak var depressionCheckButton: NSButton!
+    
+    @IBOutlet weak var hearingLossCheckButton: NSButton!
+    
+    @IBOutlet weak var arthritisCheckButton: NSButton!
+    
+    @IBOutlet weak var addNeedCheckButton: NSButton!
+    
+    var foodChecked = false;
+    var transportationChecked = false;
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        needTableView.delegate = self
-        needTableView.dataSource = self
+
         // Do any additional setup after loading the view.
-    }
-    func reloadFileList() {
-        directoryItems = directory?.contentsOrderedBy(sortOrder, ascending: sortAscending)
-        needTableView.reloadData()
     }
 
     override var representedObject: Any? {
@@ -35,63 +41,17 @@ class ViewController: NSViewController {
         }
     }
 
+    @IBAction func ActionListener(_ sender: Any) {
+        if foodChecked == false{
+            foodChecked = true
+            foodCheckButton.image = #imageLiteral(resourceName: "guy")
 
-}
-
-extension ViewController: NSTableViewDataSource {
-    
-    func numberOfRows(in tableView: NSTableView) -> Int {
-        return directoryItems?.count ?? 0
-    }
-    
-}
-
-extension ViewController: NSTableViewDelegate {
-    
-    fileprivate enum CellIdentifiers {
-        static let NeedID = "NameCellID"
-        static let SolutionID = "DateCellID"
-//        static let SizeCell = "SizeCellID"
-    }
-    
-    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-        
-        var image: NSImage?
-        var text: String = ""
-        var cellIdentifier: String = ""
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .long
-        dateFormatter.timeStyle = .long
-        
-        // 1
-        guard let item = directoryItems?[row] else {
-            return nil
         }
-        
-        // 2
-        if tableColumn == tableView.tableColumns[0] {
-            image = item.icon
-            text = item.name
-            cellIdentifier = CellIdentifiers.NeedID
-        } else if tableColumn == tableView.tableColumns[1] {
-            text = dateFormatter.string(from: item.date)
-            cellIdentifier = CellIdentifiers.SolutionID
-        } 
-//            else if tableColumn == tableView.tableColumns[2] {
-//            text = item.isFolder ? "--" : sizeFormatter.string(fromByteCount: item.size)
-//            cellIdentifier = CellIdentifiers.SizeCell
-//        }
-        
-        // 3
-        if let cell = tableView.make(withIdentifier: cellIdentifier, owner: nil) as? NSTableCellView {
-            cell.textField?.stringValue = text
-            cell.imageView?.image = image ?? nil
-            return cell
+        else{
+            foodChecked = false
+            foodCheckButton.image = #imageLiteral(resourceName: "long_term_plan")
         }
-        return nil
     }
-    
-}
 
+}
 
